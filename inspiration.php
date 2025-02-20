8186ff07841a7cb5e6627e0b99023a5d3933d81f
dan-seng <da16gi@gmail.com>
2025-02-20 23:21:52 +0300

    Add inspiration php file

---
+++ inspiration.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+    }
+    h1 {
+      text-align: center;
+      color: #FFC107;
+    }
+    h3 {
+      text-align: center;
+      color: #FFC107;
+    }
+    p {
+      text-align: center;
+      margin: 20px;
+    }
+   
+  </style>
+  <title>Inspiration</title>
+</head>
+<body>
+  <?php include 'header.php'; ?>
+  <div class="image-section">
+    <img src="images/hotel9.jpg" alt="">
+  </div>
+  <h3>
+  Aruntara Riverside Boutique Hotel in Chiang Mai 
+  </h3>
+  <h1>
+ Hotel Story
+  </h1>
+
+  <p>
+  Elegance, relaxation and peacefulness come together to create Aruntara Riverside Boutique Hotel, which is conveniently located on the banks
+  of the Mae Ping River. The design of the hotel is an architectural marriage between British Colonial and Northern Thai influences, which can be displayed by the traditional
+    Lanna style handcrafted teak columns and high ceilings throughout the hotel.
+  </p>
+  <p>
+Every piece of furniture in the hotel is exclusively designed and traditionally handmade by local Thai craftsmen, which enhances the overall Lanna (Northern Thai) concept. All of the rooms are portrayed by one of the Five classical elements (Earth, Wind, Fire, Water and Wood) which brings peace and tranquility to this Buddhist cultural theme. Aruntara Riverside Boutique Hotel connects the present with memorable anecdotes from the past. Guests can easily commute between the tranquility of Aruntara Riverside Boutique Hotel and the nearby inner city of Chiang Mai using local or organised transportation.
+
+</p>
+<?php include 'footer.php'; ?>
+
+</body>
+</html>
