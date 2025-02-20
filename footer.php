7a92ddbf209af503e4e9a813688aed6664c02fea
dan-seng <da16gi@gmail.com>
2025-02-20 23:22:52 +0300

    Add php files header and footer to be included

---
+++ footer.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<section class="contact-us-section">
+  <h2>CONTACT US</h2>
+  <p>Aruntera Riverside Boutique Hotel</p>
+  <p>355/1 Charoen Prathet Rd., Changklan, Muang, Chiang Mai 50100 Thailand</p>
+  <ul class="contact-details">
+      <li><strong>Admin:</strong> <a href="mailto:da16gi@gmail.com">da16gi@gmail.com</a></li>
+      <li><strong>Reservation:</strong> <a href="mailto:rsvn@aruntarahotel.com">rsvn@luxuryhotel.com</a></li>
+      <li><strong>Line ID:</strong> 0871108008</li>
+      <li><strong>Mobile:</strong><a href="tel:+945012123">+251945012123</a>  </li>
+     
+  </ul>
+</section>
+
+
+<footer class="footer-sec">
+  <div class="navigation-linkis">
+      <a href="#">Rooms</a> | 
+      <a href="#">Tara Restaurant</a> | 
+      <a href="#">Four Elements Spa</a> | 
+      <a href="#">Facilities</a> | 
+      <a href="#">Promotion</a> | 
+      <a href="#">Book Now</a>
+  </div>
+  <p class="footer-note">Site Powered by ACE Man</p>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <style>
+        .sub-nav button{
+  background-color: #FFC107;
+  border: none;
+  color: black;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: 0.3s;
+  font-family: 'Quicksand', serif;
+  font-optical-sizing: auto;
+  font-style: normal;
+  font-size: 18px;
+}
+
+.sub-nav button:hover {
+  background-color: #FF5722;
+}
+.sub-nav {
+  text-align: center;
+  margin-bottom: 50px;
+}
+    </style>
+  </head>
+<body>
+
+
+<nav class="navbar">
+    <div class="logo">
+        <img src="logo.png" alt="Hotel Logo">
+        <span style=" font-family: 'Quicksand', serif;font-optical-sizing: auto;;font-style: normal;"  
+        >Luxury Hotel</span>
+
+    </div>
+    <ul class="nav-links">
+        <li><a href="index.php" class="active">Home</a></li>
+        <li><a href="inspiration.php">Inspiration</a></li>
+        <li><a href="gallery.php">Gallery</a></li>
+    </ul>
+</nav>
+
+<div class="sub-nav">
+    <button onclick="window.location.href='book.php'"
+    >Book now</button>
+</div>
+
+</body>
+</html>
